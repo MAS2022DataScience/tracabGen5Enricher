3b7071f7e4ad590cbe849c76ff0af3f671bf2f7d
Pascal Hubacher <pascal.hubacher@outlook.com>
2023-03-19 15:11:27 +0100

    docker image ready

--- dockerize.cmd
+++ dockerize.cmd
@@ -1,6 +1,8 @@
-docker build -t tracabGen5Enricher:0.0.1 -t tracabGen5Enricher:latest .
+REM docker build -t tracabgen5enricher:0.0.1 -t tracabgen5enricher:latest .
+docker rmi tracabgen5enricher:latest -f
+docker build -t tracabgen5enricher:latest .
 
 REM if it runs on localhost
-REM docker run -it -p 8080:8080 tracabGen5Enricher:latest
+REM docker run -it -p 8080:8080 tracabgen5enricher:latest
 REM if it runs on 192.168.1.100
-REM docker run --env DATAPLATFORM_IP=192.168.1.100 -it -p 8080:8080 tracabGen5Enricher:latest
+REM docker run --env DATAPLATFORM_IP=192.168.1.100 -it -p 8080:8080 tracabgen5enricher:latest
